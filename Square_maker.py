2f2297cac371587613304d6bdcf9850a8c3991e8
sholguinASC <holguinsteven550@gmail.com>
2016-07-13 09:23:19 -0400

    Update Square_maker.py

--- Square_maker.py
+++ Square_maker.py
@@ -3,7 +3,8 @@ init("sim")
 length = int(raw_input("How big do you want the square? "))
 penDown()
 for i in range(3):
+  for i in range(3):
+    forward(1,length)
+    turnBy(90,"deg")
   forward(1,length)
-  turnBy(90,"deg")
-  
-forward(1,length)
+  length = length*1.5
